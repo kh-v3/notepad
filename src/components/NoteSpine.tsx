9388e27e1b2b39784c269af51250f264e76e017d
kh_h <init.kh.v3@gmail.com>
2023-03-13 15:10:00 +0900

    라인 정리

--- src/components/NoteSpine.tsx
+++ src/components/NoteSpine.tsx
@@ -59,34 +59,48 @@ function NoteSpine() {
       </div>
       <div className="spine__title" id="title">lkh's notepad</div>
       <div className="spine__side spine__side--right">
-        {user && currentPage === 'list' && !isMobile && <a href="/write" className="aIcon">
+      {user && currentPage === 'list' && !isMobile &&
+        <a href="/write" className="aIcon">
           <img alt="" className="svg--pen-to-square-solid"/><span className="hidden-text">새 노트</span>
-        </a>}
-        {user && currentPage === 'write' && !isMobile && <button type="button" className="btnIcon svg--floppy-disk-solid">
+        </a>
+      }
+      {user && currentPage === 'write' && !isMobile &&
+        <button type="button" className="btnIcon svg--floppy-disk-solid">
           <span className="hidden-text">저장</span>
-        </button>}
-        {user && currentPage === 'detail' && !isMobile && <button type="button" className="btnIcon svg--trash-can-solid-white">
+        </button>
+      }
+      {user && currentPage === 'detail' && !isMobile &&
+        <button type="button" className="btnIcon svg--trash-can-solid-white">
           <span className="hidden-text">삭제</span>
-        </button>}
-        {user && currentPage === 'list' && isMobile && <button type="button" className="btnIcon svg--bars-solid">
+        </button>
+      }
+      {user && currentPage === 'list' && isMobile &&
+        <button type="button" className="btnIcon svg--bars-solid">
           <span className="hidden-text">메뉴</span>
-        </button>}
-        {user && currentPage === 'list' && isMobile && <button type="button" className="btnIcon svg--xmark-solid-white">
+        </button>
+      }
+      {user && currentPage === 'list' && isMobile &&
+        <button type="button" className="btnIcon svg--xmark-solid-white">
           <span className="hidden-text">닫기</span>
-        </button>}
+        </button>
+      }
         <div className="menu-list menu-list__type--menu hide">
-          {user && currentPage === 'list' && isMobile && <div className="menu moveWriteNote">
+        {user && currentPage === 'list' && isMobile &&
+          <div className="menu moveWriteNote">
             <button type="button" className="btnIcon svg--pen-to-square-solid">
               <span className="hidden-text">새 노트</span>
             </button>
             <div className="menu__name">새 노트</div>
-          </div>}
-          {user && currentPage === 'list' && isMobile && <div className="menu" id="showBookmarkList">
+          </div>
+        }
+        {user && currentPage === 'list' && isMobile &&
+          <div className="menu" id="showBookmarkList">
             <button type="button" className="btnIcon svg--bookmark-solid">
               <span className="hidden-text">북마크</span>
             </button>
             <div className="menu__name">북마크</div>
-          </div>}
+          </div>
+        }
         </div>
       </div>
     </header>
